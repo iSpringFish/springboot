5f1f1f4b755c94d2e099f5a139136bb263d88f7c
iSpringFish <ahyuquan@126.com>
2018-11-26 17:37:56 +0800

    hello world

--- chapter1/src/main/java/cn/springfish/chapter1/Chapter1Application.java
+++ chapter1/src/main/java/cn/springfish/chapter1/Chapter1Application.java
@@ -2,10 +2,17 @@ package cn.springfish.chapter1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Chapter1Application {
 
+    @RequestMapping("/hi")
+    public String index(){
+        return "hello, this spring boot .";
+    }
     public static void main(String[] args) {
         SpringApplication.run(Chapter1Application.class, args);
     }
